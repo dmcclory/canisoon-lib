f2df221a766dea21f6edd2736857b52446b10a93
Dan <danmcclory@gmail.com>
2021-10-03 22:40:36 -0400

    remove an unused method

--- src/loaders.ts
+++ src/loaders.ts
@@ -2,23 +2,6 @@ import { readFileSync } from 'fs';
 
 import { Feature } from './feature'
 
-export const loadDataset = (path: string): Feature[] => {
-
-  const data = readFileSync(path)
-  const res = JSON.parse(data.toString())
-
-  return Object.entries(res.data).map(data => {
-    const [k, d]: [string, any] = data;
-    return {
-      slug: k,
-      name: d.title,
-      latestPercentage: d.usage_perc_y,
-      description: d.description,
-      categories: d.categories,
-    }
-  })
-}
-
 export const loadRawDataset = (path: string) => {
   const data = readFileSync(path)
   const res = JSON.parse(data.toString())
