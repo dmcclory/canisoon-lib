bc58fe2d562a079e8a012d3f0a29706dd9fcfd8d
Dan <danmcclory@gmail.com>
2021-10-04 23:14:15 -0400

    merge the data from a number of files

--- src/loaders.ts
+++ src/loaders.ts
@@ -1,4 +1,4 @@
-import { readFileSync } from 'fs';
+import { readFileSync, readdirSync } from 'fs';
 
 import { Feature } from './feature';
 
@@ -8,4 +8,17 @@ export const loadRawDataset = (path: string): CaniuseData => {
   const data = readFileSync(path)
   const res = JSON.parse(data.toString())
   return res.data;
+}
+
+
+export const findDatasets = (path: string): {date: Date, path: string}[] => {
+  let jsonFiles: string[] = readdirSync(path).filter(f => f.match(/\.json/))
+
+  let withRawDates = jsonFiles.map( f => {
+    let d = f.match(/^(\d+)_(\d+)_(\d+)-/)!
+    let date = new Date(Number(d[1]), Number(d[2])-1, Number(d[3]))
+    return { date, path: path + '/' + f}
+  })
+
+  return withRawDates
 }
--- start.ts
+++ start.ts
@@ -1,6 +1,8 @@
 import { buildFromFile } from './src/builder';
 import { Feature, asLine, detailView} from './src/feature';
 
+import { findDatasets } from './src/loaders';
+
 
 // obviously I should extract the dates from the file name
 // (or git, eventually)
@@ -11,7 +13,9 @@ let datasets = [
  { date: new Date(2021, 10, 26),  path: 'data/2021_09_26-data-2.0.json' },
 ]
 
-const dataset = buildFromFile(datasets)
+// findDatasets('data/')
+
+const dataset = buildFromFile(findDatasets('data/'))
 
 Object.values(dataset).slice(0, 10).map( f => console.log(asLine(f)) );
 
