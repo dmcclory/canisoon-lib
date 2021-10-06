8a7e5b55719acc690f7811811b9e5fbf2f65afaa
Dan <danmcclory@gmail.com>
2021-10-05 23:16:00 -0400

    rework the demo into a handful of functions

--- gitdemo.ts
+++ gitdemo.ts
@@ -3,19 +3,32 @@ import simpleGit from 'simple-git';
 import { rm } from  'fs/promises';
 import { existsSync } from  'fs';
 
+const REPO_URL = 'https://github.com/Fyrd/caniuse';
+
 const git = simpleGit();
 
-const url = 'https://github.com/Fyrd/caniuse';
-
-if ( !existsSync('repodata/.git/index')) {
-  await rm('./repodata', { recursive: true, force: true})
-  await git.clone(url, 'repodata')
+export const fetchRepo = async () => {
+  if ( !existsSync('repodata/.git/index')) {
+    await rm('./repodata', { recursive: true, force: true})
+    await git.clone(REPO_URL, 'repodata')
+  }
 }
 
 
-// set the current working directory
-await git.cwd({ path: 'repodata', root: true})
+export const fetchAllHashes = async () => {
+  await git.cwd({ path: 'repodata', root: true})
+  const logs = await git.log()
+  return logs.all.map(l => { return { date: l.date, hash: l.hash }})
+}
 
-const neat = await git.log({maxCount: 300})
 
-console.log("loggs! ", Object.keys(neat.all))
+
+const awesome = async () => {
+  await fetchRepo();
+
+  const hashes = await fetchAllHashes()
+
+  console.log(hashes.slice(0, 6))
+}
+
+awesome()
